1f95418fb832d7cbdca24da2a0bbcc28c63d3b19
swlxzy <karaaslanz282@gmail.com>
2026-09-11 03:26:13 +0300

    Implement RecyclerView in MainActivity

--- app/src/main/java/com/yumi/player/MainActivity.kt
+++ app/src/main/java/com/yumi/player/MainActivity.kt
@@ -1,1 +1,24 @@
+package com.yumi.player
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var videoAdapter: VideoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerViewVideos)
+
+        videoAdapter = VideoAdapter(emptyList())
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = videoAdapter
+    }
+}
